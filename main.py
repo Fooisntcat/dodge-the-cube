c8b1283b6920fe28b64a4852331a58f5826d5039
Fooisntcat <Fooisntcat@users.noreply.github.com>
2024-09-04 17:52:17 +0800

    Initialize pygame

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+import random
+
+# Initialize Pygame
+pygame.init()
+
+# Set screen dimensions
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# Define colors Â 
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
